02d94b3c9e93bbc5e8568bf0c5d927c9aa56d99b
LucaAbdaladze <96344933+LucaAbdaladze@users.noreply.github.com>
2024-12-04 11:25:55 +0000

    Add files via upload

--- manageit/config.php
+++ manageit/config.php
@@ -1,17 +1,5 @@
-<!-- config.php -->
 <?php
-$host = 'localhost';
-$db = 'manageit';
-$user = 'jaelyn'; // my sql username
-$pass = '8150'; // my password
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
+$conn = mysqli_connect('localhost','root','','user_db');
 
 ?>
-<!-- config page is for connecting to the database
-and so that we dont have to rewrite this on every php page -->
