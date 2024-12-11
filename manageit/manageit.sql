72a8e0ffba8d8f9c8275294d57c75ac35dde02d9
jaelynnnn <chimnonyeremdisu@gmail.com>
2024-12-11 00:31:04 +0000

    Update manageit.sql

--- manageit/manageit.sql
+++ manageit/manageit.sql
@@ -1,33 +1,32 @@
--- Drop database if it already exists
+-- drop the database incase it already exists
 DROP DATABASE IF EXISTS manageit;
 
--- Create the database
+-- create the database
 CREATE DATABASE manageit;
 
--- Use the database "manageit"
+-- use the database manageit
 USE manageit;
 
--- Company Table
+-- create table for company data
 CREATE TABLE company (
     company_id INT AUTO_INCREMENT,
     company_name VARCHAR(100) NOT NULL,
-    company_pass VARCHAR(50) NOT NULL,
+    company_pass VARCHAR(255) NOT NULL, 
     company_email VARCHAR(100) NOT NULL,
-    company_key VARCHAR(50) NOT NULL,
+    company_key VARCHAR(50) NOT NULL UNIQUE,
     PRIMARY KEY (company_id)
 );
 
--- Staff Table
+-- create table for staff data
 CREATE TABLE staff (
     staff_id INT AUTO_INCREMENT,
     staff_name VARCHAR(100) NOT NULL,
     staff_email VARCHAR(100) NOT NULL,
-    company_key VARCHAR(50),
-    PRIMARY KEY (staff_id),
-    FOREIGN KEY (company_key) REFERENCES company(company_key) ON DELETE CASCADE
+    staff_key VARCHAR(50) NOT NULL,
+    PRIMARY KEY (staff_id)
 );
 
--- Projects Table
+-- create table for project data
 CREATE TABLE projects (
     project_id INT AUTO_INCREMENT,
     project_title VARCHAR(100) NOT NULL,
@@ -36,4 +35,13 @@ CREATE TABLE projects (
     project_end DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (project_id)
+);
+
+-- create table for tasks data
+CREATE TABLE tasks (
+    tasks_id INT AUTO_INCREMENT,
+    tasks_name VARCHAR(100) NOT NULL,
+    tasks_start DATE NOT NULL,
+    tasks_end DATE NOT NULL,
+    PRIMARY KEY (tasks_id)
 );
